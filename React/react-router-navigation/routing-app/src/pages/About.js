0f777663a1025681d9e6236d584f79ddd33c99fe
dibdas <dibyandu08das@gmail.com>
2022-12-16 19:30:17 +0530

    commit

--- React/react-router-navigation/routing-app/src/pages/About.js
+++ React/react-router-navigation/routing-app/src/pages/About.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function About() {
-  return <div>About</div>;
+  const location = useLocation();
+  const data = location.state;
+  console.log(data);
+
+  return (
+    <div>
+      About<p>{data.from}</p>
+      <p>{data.anykey}</p>
+      <p>{data.luckynumber}</p>
+    </div>
+  );
 }
 
 export default About;
