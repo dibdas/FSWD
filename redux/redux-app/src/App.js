548d13d5d8a6b2ef9599f5a1ccdfe014d5119d03
dibdas <dibyandu08das@gmail.com>
2022-12-21 00:32:21 +0530

    modified

--- redux/redux-app/src/App.js
+++ redux/redux-app/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Child1 from "./components/Child1";
 import Child2 from "./components/Child2";
+import Navbar from "./components/e-commerce/Navbar";
+import ProductList from "./components/e-commerce/ProductList";
 import CreateNotes from "./components/notes/CreateNotes";
 import ListNotes from "./components/notes/ListNotes";
 
@@ -9,8 +11,10 @@ function App() {
     <div className="App">
       {/* <Child1 />
       <Child2 /> */}
-      <CreateNotes />
-      <ListNotes />
+      {/* <CreateNotes />
+      <ListNotes /> */}
+      <Navbar />
+      <ProductList />
     </div>
   );
 }
--- redux/redux-app/src/index.js
+++ redux/redux-app/src/index.js
@@ -8,12 +8,14 @@ import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     {/* attaching the store with the app , by the help of Provider */}
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
