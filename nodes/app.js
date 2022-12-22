b68d7e9f8a7d60342ff6eae5af8a80aedbb3f09e
dibdas <dibyandu08das@gmail.com>
2022-12-23 02:11:35 +0530

    modified

--- nodes/app.js
+++ nodes/app.js
@@ -1,8 +1,16 @@
 // import xyz from "./utils.js"; // ES6 syntax
-// const multiply = require("./utils.js");  output --> { multiply: [Function: multiply] }
-const { multiply } = require("./utils.js");
 
-console.log(multiply(7, 9));
+// importing as an object
+// multiply  is an object contains the function multiple
+// const multiply = require("./utils.js");  output --> { multiply: [Function: multiply] }
+const { multiply: mul } = require("./utils.js");
+console.log(mul(7, 9));
+
+// any name can be given if not destructuring
+const multiple = require("./utils"); // common Js syntax
+// multiple is an object contains the function multiple
+console.log(multiple); // { multiply: [Function: multiply] }
+console.log(multiple.multiply(9, 6));
 
 // const a = 9;
 // const b = 8;
