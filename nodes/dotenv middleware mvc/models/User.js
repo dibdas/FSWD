5f120b10cafc4c34827726398553f4965d3a3c88
dibdas <dibyandu08das@gmail.com>
2022-12-25 23:17:46 +0530

    added files

---
+++ nodes/dotenv middleware mvc/models/User.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: `456863`,
+    email: `amit@gmail.com`,
+    password: `4678`,
+  },
+  {
+    id: `456789`,
+    email: `ranjit@gmail.com`,
+    password: `3798`,
+  },
+];
