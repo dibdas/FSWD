ae1fb6ed4bfd9a9a17a259dc40d74d94a2d0d1cb
dibdas <dibyandu08das@gmail.com>
2022-12-26 00:19:59 +0530

    modified

--- nodes/dotenv middleware mvc/routes/auth.js
+++ nodes/dotenv middleware mvc/routes/auth.js
@@ -1,14 +1,31 @@
-const router = require("express").Router();
-router.post("/login", (req, res) => {
-  res.json({
-    text: `this is for login`,
-  });
-});
+const { loginController, signupController } = require("../controllers/auth");
 
-router.post("/signup", (req, res) => {
-  res.json({
-    text: `this is for sign up`,
-  });
-});
+// breaking the code into layers to enhance its scalibility , helps in debugging
+const router = require("express").Router();
+// router.post("/login", (req, res) => {
+//   res.json({
+//     text: `this is for login`,
+//   });
+// });
+// moving the below to login controller
+// (req, res) => {
+//   res.json({
+//     text: `this is for login`,
+//   });
+// });
+router.post("/login", loginController);
+
+// router.post("/signup", (req, res) => {
+//   res.json({
+//     text: `this is for sign up`,
+//   });
+// });
+// moving the code below to signup controller
+// (req, res) => {
+//   res.json({
+//     text: `this is for sign up`,
+//   });
+// });
+router.post("/signup", signupController);
 
 module.exports = router;
