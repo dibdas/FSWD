11a06c50f37d30a7112466e7a273dbcf92aca3ae
dibdas <dibyandu08das@gmail.com>
2022-12-27 01:31:06 +0530

    modified

--- nodes/dotenv middleware mvc/routes/index.js
+++ nodes/dotenv middleware mvc/routes/index.js
@@ -1,8 +1,13 @@
 // index.js  is the entry point of any folder inside javascript
+// will use this logger middleware before it goes to the auth router
 const router = require("express").Router();
+const { logger } = require("../middlewares/Logger");
+
 console.log(router); //function
 const postRouter = require("./post");
 const authRouter = require("./auth");
+
 router.use("/post", postRouter);
-router.use("/auth", authRouter);
+// multiple middlewares can be used in here in between
+router.use("/auth", logger, authRouter);
 module.exports = router;
