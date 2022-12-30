91f02e0e91fd096b05eac033853f7046f085b075
dibdas <dibyandu08das@gmail.com>
2022-12-30 19:21:54 +0530

    modified

--- nodes/dotenv middleware mvc/models/User.js
+++ nodes/dotenv middleware mvc/models/User.js
@@ -1,13 +1,27 @@
-const users = [
-  {
-    id: `456863`,
-    email: `amit@gmail.com`,
-    password: `4678`,
+// const users = [
+//   {
+//     id: `456863`,
+//     email: `amit@gmail.com`,
+//     password: `4678`,
+//   },
+//   {
+//     id: `456789`,
+//     email: `ranjit@gmail.com`,
+//     password: `3798`,
+//   },
+// ];
+
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    // email has to pass , as it is set to required
+    required: true,
   },
-  {
-    id: `456789`,
-    email: `ranjit@gmail.com`,
-    password: `3798`,
+  marks: {
+    type: Number,
+    default: 0,
   },
-];
+  password: String,
+});
 module.exports = users;
