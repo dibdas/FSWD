b2855be8f8dbbc49d065dcd042a6880617830f47
dibdas <dibyandu08das@gmail.com>
2022-12-31 00:26:52 +0530

    modified

--- nodes/dotenv middleware mvc/models/User.js
+++ nodes/dotenv middleware mvc/models/User.js
@@ -12,16 +12,21 @@
 // ];
 
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    // email has to pass , as it is set to required
-    required: true,
-  },
-  marks: {
-    type: Number,
-    default: 0,
-  },
-  password: String,
-});
-module.exports = users;
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      // email has to pass , as it is set to required
+      required: true,
+      unique: true,
+    },
+    marks: {
+      type: Number,
+      default: 0,
+    },
+    password: String,
+  }, // moongose.Schema ends here so adding timestamp in the next line
+  { timestamps: true }
+);
+// module.exports = users;
+module.exports = mongoose.model("users", userSchema);
